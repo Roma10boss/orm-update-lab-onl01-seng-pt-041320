19331bae2276cca1bf3cd3c57537e9d61d6c9845
Romario Gustave <romagustave10@gmail.com>
2020-06-19 18:46:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,7 @@ class Student
        INSERT INTO students(name,grade)
        VALUE (?,?)
       SQL
-        DB[:conn].execute(sql, self.name, vself.grade)
+        DB[:conn].execute(sql, self.name, self.grade)
         @id = DB[:conn].execute("SELECT lat_insert_rowid() FROM students")[0][0]
     end
   end
