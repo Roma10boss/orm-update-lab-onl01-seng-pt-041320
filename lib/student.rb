b67561ff5f6e35c160ad42463279d6d4bb40de8b
Romario Gustave <romagustave10@gmail.com>
2020-06-19 18:28:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,17 @@
 require_relative "../config/environment.rb"
 
 class Student
-
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  attr_accessor :name, :grade 
+  attr_reader :id
+
+  def initialize(name,grade,grade=nil)
+    @name = name 
+    @grade - grade 
+    @id = id 
+  end 
+  
 
 
 end
