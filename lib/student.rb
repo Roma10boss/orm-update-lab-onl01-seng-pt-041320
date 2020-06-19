7795907c29c7de81de6421feba0a44e9d35b28ea
Romario Gustave <romagustave10@gmail.com>
2020-06-19 18:58:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,9 +40,17 @@ class Student
       end
     end
 
-    def self.create(name:, grade:)
+
+  end	  def self.create(name:, grade:)
       student = Student.new(name, grade)
       student.save
-       student
-     end
+      student
+    end
+
+    def self.new_from_db(row)
+      id = row[0]
+      name = row[1]
+      grade = row[2]
+      self.new(id, name, grade)
+    end
 end
